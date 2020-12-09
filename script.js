a1426b44664901e001bed00f51c56b8f536dbb60
EthanPrieur <ethan.prieur@ocsbstudent.ca>
2020-12-09 14:38:52 +0000

    Unfixed some Non-errors

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-document.getElementById('Button1').addEventListener('click', function()  {
-  alert('Thanks for clicking that!');
+document.getElementById("button1").addEventListener("click", function() {
+  alert("Thanks for clicking that!");
 });
+
     
